7760106e97dde97f2ade80798c0b29816f706619
isaquecsilva <isaque.costa9333@gmail.com>
2024-02-08 21:53:14 -0300

    QueryPlace implementation for Search field

--- frontend/src/components/Fields/cityrequest.js
+++ frontend/src/components/Fields/cityrequest.js
@@ -1,3 +1,6 @@
+import { QueryWheather } from '../../../wailsjs/go/main/WheeaApp.js'
+import sweetalert2 from 'sweetalert2'
+
 const waitTime = 3 * 1000;
 var apiCallCountdown = null;
 
@@ -6,6 +9,27 @@ export function getCityInfo(cityname) {
 		clearTimeout(apiCallCountdown);
 	}
 
-	apiCallCountdown = setTimeout(() => console.log(`calling api: ${cityname}`), 
-		waitTime)
+	if (!cityname.length) {
+		return
+	}
+
+	apiCallCountdown = setTimeout(async () => {
+		let response = await QueryWheather(cityname)
+
+		if (response.Error.length) {
+			sweetalert2.fire({
+				icon: "error",
+				title: "Error",
+				text: response.Error
+			})
+		} 
+		else {
+			var event = new CustomEvent('queryplaceresponse', { 
+				detail: {
+					data: response.Results
+				}
+			});
+			window.dispatchEvent(event)
+		}
+	}, waitTime)
 }
