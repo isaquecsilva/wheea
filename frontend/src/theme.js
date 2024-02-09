e17c259831c72457dad789081d07acbd6898ea6f
isaquecsilva <isaque.costa9333@gmail.com>
2024-02-08 21:50:30 -0300

    missing from before-previews commit

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -11,8 +11,13 @@ function setUserTheme(theme) {
 	localStorage.setItem("apptheme", theme)
 }
 
+function setComponentGridCoords(rs=1, re=1, cs=1, ce=1) {
+	return {rs,	re,	cs,	ce}
+}
+
 export {
 	themekit,
 	getUserTheme,
-	setUserTheme
+	setUserTheme,
+	setComponentGridCoords
 }
