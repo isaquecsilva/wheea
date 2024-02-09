dff2f3aa6a7161609399a709b5acfe0718887b4f
isaquecsilva <isaque.costa9333@gmail.com>
2024-02-08 21:55:09 -0300

    go binding for queryplace operation; DependencyInjection for WheeaApp struct

--- app.go
+++ app.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"context"
+	"wheea/core/app"
+	"wheea/core/entities"
 )
 
 type WheeaApp struct {
+	qps *app.QueryPlaceService
 	ctx context.Context
 }
 
 // NewApp creates a new App application struct
-func NewWheeaApp() *WheeaApp {
-	return &WheeaApp{}
+func NewWheeaApp(qps *app.QueryPlaceService) *WheeaApp {
+	return &WheeaApp{
+		qps: qps,
+	}
 }
 
 // startup is called when the app starts. The context is saved
 // so we can call the runtime methods
 func (a *WheeaApp) startup(ctx context.Context) {
 	a.ctx = ctx
+}
+
+func (a *WheeaApp) QueryWheather(cityname string) entities.QueryPlaceApiResponse {
+
+	queryplace := entities.NewQueryPlace(cityname)
+	return a.qps.Execute(queryplace)
 }
--- main.go
+++ main.go
@@ -3,30 +3,35 @@ package main
 import (
 	"embed"
 
+	"wheea/adapters"
+	"wheea/core/app"
+
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
-	_"github.com/wailsapp/wails/v2/pkg/options/mac"
-	_"github.com/wailsapp/wails/v2/pkg/options/linux"
-	"github.com/wailsapp/wails/v2/pkg/options/windows"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
 func main() {
+	// Instanciating services and adapters
+	var queryPlaceAdapter *adapters.QueryPlaceAdapter = adapters.NewQueryPlaceAdapter()
+	queryPlaceService := app.NewQueryPlaceService(queryPlaceAdapter)
+
 	// Create an instance of the app structure
-	app := NewWheeaApp()
+	app := NewWheeaApp(&queryPlaceService)
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "wheea",
-		MinWidth: 344,
-		MinHeight: 370,
-		Width:  344,
-		Height: 370,
-		MaxWidth: 600,
-		MaxHeight: 600,
+		Title:      "wheea",
+		MinWidth:   344,
+		MinHeight:  370,
+		Width:      344,
+		Height:     370,
+		MaxWidth:   600,
+		MaxHeight:  600,
 		Fullscreen: false,
 		Windows: &windows.Options{
 			IsZoomControlEnabled: false,
@@ -34,7 +39,7 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		OnStartup:        app.startup,
+		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
 		},
