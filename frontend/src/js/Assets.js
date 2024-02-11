3c4ae9cfeaa96113db99c59cbfe42b8827a83f5f
isaquecsilva <isaque.costa9333@gmail.com>
2024-02-11 19:38:28 -0300

    assets images resized for better smaller bundler and Assets.js defined to export all images for application building process

---
+++ frontend/src/js/Assets.js
@@ -0,0 +1,17 @@
+import sunnny from '../assets/images/illustrations/sunnny.png'
+import sun_cloudy from '../assets/images/illustrations/sun_cloudy.png'
+import sun_cloudy_misty from '../assets/images/illustrations/sun_cloudy_misty.png'
+import sun_rain from '../assets/images/illustrations/sun_rain.png'
+import sun_rain_bolt from '../assets/images/illustrations/sun_rain_bolt.png'
+import cloud_fog from '../assets/images/illustrations/cloud_fog.png'
+import cloudy_rain from '../assets/images/illustrations/cloudy_rain.png'
+
+export {
+    sunnny,
+    sun_cloudy,
+    sun_cloudy_misty,
+    sun_rain,
+    sun_rain_bolt,
+    cloud_fog,
+    cloudy_rain
+}
