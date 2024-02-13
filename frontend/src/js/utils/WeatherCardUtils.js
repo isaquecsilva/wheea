6ab342491b0b71423dfadb0f35c7706ad02f7ba2
isaquecsilva <isaque.costa9333@gmail.com>
2024-02-13 12:52:42 -0300

    WeatherCodeToImage multiple-switch values correct assessment (cascading) set

--- frontend/src/js/utils/WeatherCardUtils.js
+++ frontend/src/js/utils/WeatherCardUtils.js
@@ -3,20 +3,39 @@ import { sun_rain_bolt, sunny, sun_cloudy, cloud_fog, cloudy_rain } from "../Ass
 export function weatherCodeToImage(weathercode) {
     console.log(`weather code: ${weathercode}`)
     switch (weathercode) {
-        case 0||1:
+        case 0:
+        case 1:
             return sunny
         case 2:
             return sun_cloudy
         case 3:
             return cloud_fog
-        case 45||48:
+        case 45:
+        case 48:
             return cloud_fog
-        case 51||53||55||56||57||61||63||66||73||77||80||81||85:
+        case 51:
+        case 53:
+        case 55:
+        case 56:
+        case 57:
+        case 61:
+        case 63:
+        case 66:
+        case 73:
+        case 77:
+        case 80:
+        case 81:
+        case 85:
             return cloudy_rain
-        case 65||67||75||82||86||weathercode >= 95:
+        case 65:
+        case 67:
+        case 75:
+        case 82:
+        case 86:
+        case weathercode >= 95:
             return sun_rain_bolt
         default:
+            console.log('chegou aqui')
             return sunny
-
     }
 }
